e69c8dfe47980d68d0f7d4c6d3442b378bc9365a
Michael Walker <mike@barrucadu.co.uk>
2016-08-14 01:15:41 +0100

    Strip leading /s

--- src/Main.hs
+++ src/Main.hs
@@ -10,12 +10,11 @@ main :: IO ()
 main = gopher "barrucadu.co.uk" 70 serve
 
 serve :: String -> IO (Maybe Response)
+serve ('/':rest) = serve rest
+serve ('.':_) = pure Nothing
 serve ""  = Just . DirListing <$> dirFile "root.gopher"
-serve "/" = Just . DirListing <$> dirFile "root.gopher"
--- No hidden files for you!
-serve ('/':'.':_) = pure Nothing
-serve ('/':rest) = do
-  let fname = map (\c -> if c == '/' then '-' else c) rest
+serve selector = do
+  let fname = map (\c -> if c == '/' then '-' else c) selector
   fileExists   <- doesFileExist fname
   gopherExists <- doesFileExist (fname ++ ".gopher")
   if gopherExists
@@ -25,7 +24,6 @@ serve ('/':rest) = do
             then Just . Text   <$> readFile   fname
             else Just . Binary <$> B.readFile fname
        else pure Nothing
-serve _ = pure Nothing
 
 textFileExts :: [String]
 textFileExts = [".txt", ".md", ".markdown"]
